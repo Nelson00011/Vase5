886c3fb1ffadf9d0b212aad76dd06ec4771da57c
Ayla Nelson <anelson@sonos.com>
2025-09-15 09:25:38 -0700

    app stx TODO research

---
+++ AI03/app.js
@@ -0,0 +1,9 @@
+This REST API needs user authentification. 
+
+users must be able to register (signup) and Login.
+
+Don't add any JWT coe or anything like that, just generate user model without using classes) in the models folder.
+
+
+
+
